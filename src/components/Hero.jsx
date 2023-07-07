c374ee4496145a5ce90da8da361d803b968861b6
iamkunal2k <kunalkumbhare8@gmail.com>
2023-07-07 13:41:27 +0530

    completed with the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,6 +13,15 @@ const Hero = () => {
                 Github
             </button>
         </nav> 
+
+        <h1 className='head_text'>
+            Summerize Articles with <br className='max.md:hidden'/>
+            <span className='orange_gradient'>OpenAI GPT-4</span>
+        </h1>
+
+        <h2 className='desc'>
+            Simplify your reading with Summz, an open-source article Summerizer that transforms lengthy articles into clear and concise summaries
+        </h2>
     </header>
   )
 }
