5fafb2dfa8df5bf1beb662bfcd40c70374b6a7d9
iamkunal2k <kunalkumbhare8@gmail.com>
2023-07-07 23:31:59 +0530

    Done with the Browse History Section of our webpage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -13,15 +13,17 @@ const Demo = () => {
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();       //to know whether we have error, whether we are fetching to actually call to get the summary of an article
 
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
-    )
+// Load data from localStorage on mount
+useEffect(() => {
+  const articlesFromLocalStorage = JSON.parse(
+    localStorage.getItem("articles")
+  );
 
-    if(articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
-    }
-  }, [])
+  if (articlesFromLocalStorage) {
+    setAllArticles(articlesFromLocalStorage);
+  }
+}, []);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -31,11 +33,12 @@ const Demo = () => {
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
-      
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+     // update state and local storage
+     setArticle(newArticle);
+     setAllArticles(updatedAllArticles);
+
+     localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   }
   return (
@@ -70,7 +73,23 @@ const Demo = () => {
         </form>
 
         {/* Browse URL History */}
-        
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+           {allArticles.map((item,  index) => (
+            <div 
+            key={`link-${index}`}
+            onClick={() => setArticle(item)}
+            className='link_card'
+            >
+              <div className='copy_btn'> 
+                <img src={copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain' 
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
