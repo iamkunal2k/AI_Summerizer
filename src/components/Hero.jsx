b53e5aaa0ad463d236fab5dfe2882edc79177a5c
iamkunal2k <kunalkumbhare8@gmail.com>
2023-07-07 13:36:15 +0530

    Completed with creating a navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,15 @@ import {logo} from '../assets'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-        
+       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <img src={logo} alt='sumz_logo' className=' w-28 object-contain' />
+
+            <button type='button' 
+            onClick={() => window.open('https://github.com/iamkunal2k/AI_Summerizer')} 
+            className='black_btn'>
+                Github
+            </button>
+        </nav> 
     </header>
   )
 }
